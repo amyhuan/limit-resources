fb3e684e3b9257f049dfa03b3d3307396ed81224
amyhuan <amyhuan@microsoft.com>
2021-03-09 17:19:43 -0600

    Open 100 files and write short string to each

--- main.go
+++ main.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"log"
+	"fmt"
+	"os"
 )
+
+func GetCPU(jun *JuniperUtilizationReader) {
+	cpuPercent, err := jun.GetCPUPercent()
+	if err != nil {
+		log.Printf("Failed to get CPU percentage: %s", err)
+	}
+	log.Printf("Baseline CPU usage: %f%", cpuPercent)
+}
+
+func GetMem(jun *JuniperUtilizationReader) {
+	memPercent, err := jun.GetMemoryPercent()
+	if err != nil {
+		log.Printf("Failed to get CPU percentage: %s", err)
+	}
+	log.Printf("Baseline memory usage: %f%", memPercent)
+}
 
 func main() {
 	jun, err := NewJuniperUtilizationReader()
@@ -10,19 +28,33 @@ func main() {
 		log.Printf("Failed to create juniper utilization reader: %s", err)
 	}
 
-	cpuPercent, err := jun.GetCPUPercent()
-	if err != nil {
-		log.Printf("Failed to get CPU percentage: %s", err)
+	for i := 0; i < 100; i++ {
+		path := fmt.Sprintf("test-file-%v.txt", i)
+		var file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Printf("Failed to open file %s: %v", path, err)
+			break
+		}
+		defer file.Close()
+		_, err = file.WriteString(fmt.Sprintf("hello this is file %v", i))
+		if err != nil {
+			log.Printf("Failed to write to file %s: %v", path, err)
+		}
 	}
-	log.Printf("Baseline CPU usage: %f%", cpuPercent)
 
-	memPercent, err := jun.GetMemoryPercent()
-	if err != nil {
-		log.Printf("Failed to get CPU percentage: %s", err)
+	GetCPU(jun)
+	GetMem(jun)
+
+	// Delete files made
+	for i := 0; i < 100; i++ {
+		path := fmt.Sprintf("test-file-%v.txt", i)
+		err := os.Remove(path)
+		if err != nil {
+			log.Printf("Failed to delete file %s: %v", path, err)
+			break
+		}
 	}
-	log.Printf("Baseline memory usage: %f%", memPercent)
-	// Maybe do like...2 MB
-	// Perhaps get measuring stuff first
+	
 
 	// Use a lot of CPU
 	// Open large # of files
