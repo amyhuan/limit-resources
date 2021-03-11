e473c5b3edc18d27cef6cebf93622367507330cd
amyhuan <amyhuan@microsoft.com>
2021-03-11 17:30:34 -0600

    Set open file limits

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"fmt"
 	"os"
+	"golang.org/x/sys/unix"
 )
 
 func ShowCPUPercent(jun *JuniperUtilizationReader) {
@@ -38,14 +39,24 @@ func ShowMem(jun *JuniperUtilizationReader) {
 	log.Printf("Memory usage: %.0f MB", val)
 }
 
+func ShowUsages(jun *JuniperUtilizationReader) {
+	ShowCPUPercent(jun)
+	ShowMemPercent(jun)
+	ShowCPU(jun)
+	ShowMem(jun)
+}
+
 func main() {
 	jun, err := NewJuniperUtilizationReader()
 	if err != nil {
 		log.Printf("Failed to create juniper utilization reader: %s", err)
 	}
 
-	log.Printf("Creating 10000 files and writing short string to them")
-	numFiles := 10000
+	log.Printf("Starting up. Taking baseline stats")
+	ShowUsages(jun)
+
+	numFiles := 60
+	log.Printf("Creating %v files and writing short string to them", numFiles)
 	for i := 0; i < numFiles; i++ {
 		path := fmt.Sprintf("test-file-%v.txt", i)
 		var file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
@@ -61,10 +72,54 @@ func main() {
 		}
 	}
 
-	ShowCPUPercent(jun)
-	ShowMemPercent(jun)
-	ShowCPU(jun)
-	ShowMem(jun)
+	ShowUsages(jun)
+
+	log.Printf("Cleaning up 100 files")
+	for i := 0; i < numFiles; i++ {
+		path := fmt.Sprintf("test-file-%v.txt", i)
+		err := os.Remove(path)
+		if err != nil {
+			log.Printf("Failed to delete file %s: %v", path, err)
+			break
+		}
+	}
+
+	
+	var rLimit unix.Rlimit
+	err = unix.Getrlimit(unix.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Printf("Couldn't get rLimit: %v", err)
+		return
+	}
+	log.Printf("Current limit on open files: %v", rLimit.Cur)
+	log.Printf("Max limit on open files: %v", rLimit.Max)
+
+	newFileLimit := uint64(20)
+	log.Printf("Limiting number of open files to %v", newFileLimit)
+	rLimit.Cur = newFileLimit
+	err = unix.Setrlimit(unix.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Printf("Couldn't set rLimit: %v", err)
+		return
+	}
+
+	log.Printf("Creating %v files and writing short string to them", numFiles)
+	for i := 0; i < numFiles; i++ {
+		path := fmt.Sprintf("test-file-%v.txt", i)
+		var file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Printf("Failed to open file %s: %v", path, err)
+			break
+		}
+		defer file.Close()
+		// About 20 bytes, so 10^5 of these is about .2MB
+		_, err = file.WriteString(fmt.Sprintf("hello this is file %v", i))
+		if err != nil {
+			log.Printf("Failed to write to file %s: %v", path, err)
+		}
+	}
+
+	ShowUsages(jun)
 
 	log.Printf("Cleaning up 100 files")
 	for i := 0; i < numFiles; i++ {
